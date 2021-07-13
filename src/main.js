224975814a8557f2cf91567fe3a094c860bd10af
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-07-13 18:37:59 +0300

    push files

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 const translate = require('@vitalets/google-translate-api');
 
+class Translator {
+	async #translator (word , to){
+         let {text} = await translate(word , {to})
+         return await text
+	}
+    
+    async #translateFromOneToAnyLang (word ,from ,  to){
+         let {text} = await translate(word ,{from , to})
+         return await text
+	}
+     translateTo(word , to){
+    	return  this.#translator(word , to)
+    }
+
+}
+
+;(async function(){
+	let translator = new Translator()
+	let res = await translator.translateTo("onajonim , sizni yaxshi ko'raman" , "en")
+	let res1 = await translator.translateTo(res , "ru" , 'en')
+    console.log(res)
+    console.log(res1)
+})()
